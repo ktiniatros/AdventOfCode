636c8698ddb219ba22a2e02f0e4bdb971a6b52e5
Giorgos <giorgos@giorgos.nl>
2020-12-13 15:56:03 +0100

    Day 13

--- java/nl/giorgos/adventofcode/y2020/Day13.kt
+++ java/nl/giorgos/adventofcode/y2020/Day13.kt
@@ -4,10 +4,14 @@ import nl.giorgos.adventofcode.Utils
 import org.junit.Test
 import java.math.BigInteger
 import java.nio.file.InvalidPathException
+import java.util.*
 
 class Day13 {
     private val fileName = "input13.txt"
-    private val input = this.javaClass.getResourceAsStream(fileName) ?: throw InvalidPathException( fileName, "Invalid input")
+    private val input = this.javaClass.getResourceAsStream(fileName) ?: throw InvalidPathException(
+        fileName,
+        "Invalid input"
+    )
     private val inputText = Utils.convertFileToString(input).split("\n")
     private val lines = inputText.subList(0, inputText.size - 1)
 
@@ -30,29 +34,122 @@ class Day13 {
         val before = System.currentTimeMillis()
         val busIds = lines[1].split(',')
         val invalid = (-1).toBigInteger()
-        val offsets = busIds.mapIndexed { index, s -> Pair(index.toBigInteger(), if (s == "x") {
-            invalid
-        } else {
-            s.toBigInteger()
-        }) }.filter { it.second != invalid }.sortedByDescending { it.second }
+        val offsets = busIds.mapIndexed { index, s -> Pair(
+            index.toBigInteger(), if (s == "x") {
+                invalid
+            } else {
+                s.toBigInteger()
+            }
+        ) }.filter { it.second != invalid }.sortedByDescending { it.second }
         println(offsets)
 
 
         var condition = false
 
-        var factor = 1.toBigInteger()
+        val step = 1.toBigInteger()
+        var factor = step
         val base = offsets.first().second - offsets.first().first
         var currentTimestamp = base * factor + ((factor - 1.toBigInteger()) * offsets.first().first)
         while(!condition) {
             currentTimestamp = base * factor + ((factor - 1.toBigInteger()) * offsets.first().first)
             condition = offsets.map {
+                println("ypoloipo: ${(currentTimestamp + it.first) % it.second}")
                 (currentTimestamp + it.first) % it.second
             }.none { it != 0.toBigInteger() }
-            factor += 1.toBigInteger()
+            factor += step
+            println("currentTimestamp: $currentTimestamp")
         }
 
         println("Took; ${System.currentTimeMillis() - before}")
         println(currentTimestamp)
 
+    }
+
+    @Test
+    fun ex22() {
+        val before = System.currentTimeMillis()
+        val busIds = lines[1].split(',')
+        val invalid = (-1).toBigInteger()
+        val offsets = busIds.mapIndexed { index, s -> Pair(
+            index.toBigInteger(), if (s == "x") {
+                invalid
+            } else {
+                s.toBigInteger()
+            }
+        ) }.filter { it.second != invalid }.map { Pair(it.first.toInt(), it.second.toInt()) }
+
+
+//        println(gcd(arrayOf(7, 14)))
+//        println(gcd(arrayOf(14, 7)))
+//        println(lcm(arrayOf(7, 14)))
+//        println(lcm(arrayOf(14, 7)))
+        println((gcdEx2(Pair(0, 7), Pair(2, 6))))
+
+    }
+
+    private fun gcdEx2(x: Pair<Int, Int>, y: Pair<Int, Int>): Int {
+        return if (y.second == 0) x.second else gcdEx2(y, Pair(x.first, (x.first + x.second) % (y.second - y.first)))
+    }
+
+    private fun gcd(x: Int, y: Int): Int {
+        return if (y == 0) x else gcd(y, x % y)
+    }
+//
+//    fun gcd(vararg numbers: Int): Int {
+//        return Arrays.stream(numbers).reduce(0) { x, y -> gcd(x, y) }
+//    }
+
+    fun gcd(numbers: Array<Int>) = numbers.reduce { x, y -> gcd(x, y) }
+
+    fun lcm(numbers: Array<Int>) = numbers.reduce { x, y -> x * (y / gcd(x, y)) }
+//    fun lcm(vararg numbers: Int): Int {
+//        return Arrays.stream(numbers).reduce(1) { x, y -> x * (y / gcd(x, y)) }
+//    }
+
+    fun List<BigInteger>.sum(): BigInteger = this.foldRight(0.toBigInteger()) { acc, next ->
+        acc + next
+    }
+
+    @Test
+    fun pamepali() {
+        val before = System.currentTimeMillis()
+        val busIds = lines[1].split(',')
+        val invalid = (-1).toBigInteger()
+        val offsets = busIds.mapIndexed { index, s -> Pair(
+            index.toBigInteger(), if (s == "x") {
+                invalid
+            } else {
+                s.toBigInteger()
+            }
+        ) }.filter { it.second != invalid }.sortedBy { it.second }.map { Pair(it.first.toInt(), it.second.toInt()) }
+
+        println(offsets)
+
+        var c = offsets[0].second
+        for (i in offsets.indices) {
+            for (y in i+1 until offsets.size) {
+                c = leastCommon(offsets[i], offsets[y], c)
+            }
+        }
+
+        println(c)
+
+    }
+
+    private fun leastCommon(pair1: Pair<Int, Int>, pair2: Pair<Int, Int>, c: Int): Int {
+        var current = c
+        var c1 = current - pair1.first
+        var c2 = current - pair2.first
+        var check1 = c1 != 0 && c1 % pair1.second == 0
+        var check2 = c2 != 0 && c2 % pair2.second == 0
+        while (!(check1 && check2)) {
+            c1 = current - pair1.first
+            c2 = current - pair2.first
+//            println("pame: $current $c2")
+            check1 = c1 != 0 && c1 % pair1.second == 0
+            check2 = c2 != 0 && c2 % pair2.second == 0
+            current += 1
+        }
+        return current - 1
     }
 }

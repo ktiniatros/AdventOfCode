058e12d6aa8fa4e34d6b9c2d79f80eb80d4545c3
Giorgos <giorgos@giorgos.nl>
2020-12-18 15:00:32 +0100

    Day 18

--- java/nl/giorgos/adventofcode/y2020/Day18.kt
+++ java/nl/giorgos/adventofcode/y2020/Day18.kt
@@ -15,97 +15,39 @@ class Day18 {
 
     @Test
     fun ex1() {
-        val result = lines.map(::parseLine).sum()
+        val result = lines.map {
+            parseLine(it) { numbers, operators, res ->
+                var result = res
+                while (operators.isNotEmpty()) {
+                    val nextOperator = operators.first()
+                    result = if (nextOperator == '+') {
+                        numbers[0] + numbers[1]
+                    } else {
+                        numbers[0] * numbers[1]
+                    }
+                    operators.removeAt(0)
+                    numbers.removeAt(0)
+                    numbers.removeAt(0)
+                    numbers.add(0, result)
+                }
+                result
+            }
+        }.sum()
         //689976794 too low
         //202553439706
         println(result)
+
     }
 
-    private fun parseLine(line: String): Long {
-//        println("Parsing: $line")
+    private fun parseLine(line: String, cb: (MutableList<Long>, MutableList<Char>, Long) -> Long): Long {
         var result = 1L
-        var lastOperator = '*'
-        var amountOfOpenParenthesis = 0
-        val pendingSubResult = mutableListOf<Char>()
-        line.forEachIndexed { index, it ->
-
-            if (amountOfOpenParenthesis > 0) {
-
-                if (it == ')') {
-                    val previousParenthesisCount = amountOfOpenParenthesis
-                    amountOfOpenParenthesis -= 1
-                    if (amountOfOpenParenthesis == 0) {
-                        if (previousParenthesisCount > 0 && amountOfOpenParenthesis == 0) {
-//                            println("edw? ${pendingSubResult.joinToString("")}")
-                            val thisDigit = parseLine(pendingSubResult.joinToString(""))
-                            result = if (lastOperator == '*') {
-                                result * thisDigit
-                            } else {
-                                result + thisDigit
-                            }
-                        }
-                        pendingSubResult.clear()
-                    } else {
-                        pendingSubResult.add(it)
-                    }
-
-                } else {
-                    if (it == '(') {
-                        amountOfOpenParenthesis += 1
-                    }
-                    pendingSubResult.add(it)
-//                    println(pendingSubResult.joinToString(""))
-                }
-
-
-            } else {
-                if (it.isDigit()) {
-                    val thisDigit = Character.getNumericValue(it)
-//                    println("Prin: $previousDigit, twra: $thisDigit")
-                    result = if (lastOperator == '*') {
-                        result * thisDigit
-                    } else {
-                        result + thisDigit
-                    }
-//                    println("twra result: $result")
-                } else {
-                    when (it) {
-                        ' ' -> Unit
-                        '+' -> {
-                            lastOperator = '+'
-                        }
-                        '*' -> {
-                            lastOperator = '*'
-                        }
-                        '(' -> {
-                            amountOfOpenParenthesis += 1
-                        }
-                        ')' -> {
-                            throw IllegalStateException("Den eprese edw na er8ei")
-                        }
-                        else -> {
-                            throw IllegalStateException("Invalid operator: $it")
-                        }
-                    }
-                }
-            }
-
-
-        }
-        return result
-    }
-
-    private fun parseLine2(line: String): Long {
-//        println("Parsing: $line")
-        var result = 1L
-        var lastOperator = '*'
         var amountOfOpenParenthesis = 0
         val pendingSubResult = mutableListOf<Char>()
 
         val numbers = mutableListOf<Long>()
         val operators = mutableListOf<Char>()
 
-        line.forEachIndexed { index, it ->
+        line.forEachIndexed { _, it ->
 
             if (amountOfOpenParenthesis > 0) {
 
@@ -114,7 +56,7 @@ class Day18 {
                     amountOfOpenParenthesis -= 1
                     if (amountOfOpenParenthesis == 0) {
                         if (previousParenthesisCount > 0 && amountOfOpenParenthesis == 0) {
-                            val thisDigit = parseLine2(pendingSubResult.joinToString(""))
+                            val thisDigit = parseLine(pendingSubResult.joinToString(""), cb)
                             numbers.add(thisDigit)
                         }
                         pendingSubResult.clear()
@@ -159,37 +101,38 @@ class Day18 {
 
         }
 
-//        println(numbers)
-//        println(operators)
-        while(operators.isNotEmpty()) {
-            while(operators.contains('+')) {
-                val plusIndex = operators.indexOf('+')
-                result = numbers[plusIndex] + numbers[plusIndex + 1]
-                operators.removeAt(plusIndex)
-                numbers.removeAt(plusIndex)
-                numbers.removeAt(plusIndex)
-                numbers.add(plusIndex, result)
-            }
-            if (operators.isNotEmpty()) {
-//                println(operators)
-//                println("now multiply: $numbers")
-                val multiplyIndex = operators.indexOf('*')
-//                println(multiplyIndex)
-                result = numbers[multiplyIndex] * numbers[multiplyIndex + 1]
-                operators.removeAt(multiplyIndex)
-                numbers.removeAt(multiplyIndex)
-                numbers.removeAt(multiplyIndex)
-                numbers.add(multiplyIndex, result)
-            }
-        }
-
-//        println(result)
+        result = cb(numbers, operators, result)
 
         return result
     }
 
     @Test
     fun ex2() {
-        println(lines.map(::parseLine2).sum())
+        val result = lines.map {
+            parseLine(it) { numbers, operators, res ->
+                var result = res
+                while (operators.isNotEmpty()) {
+                    while (operators.contains('+')) {
+                        val plusIndex = operators.indexOf('+')
+                        result = numbers[plusIndex] + numbers[plusIndex + 1]
+                        operators.removeAt(plusIndex)
+                        numbers.removeAt(plusIndex)
+                        numbers.removeAt(plusIndex)
+                        numbers.add(plusIndex, result)
+                    }
+                    if (operators.isNotEmpty()) {
+                        val multiplyIndex = operators.indexOf('*')
+                        result = numbers[multiplyIndex] * numbers[multiplyIndex + 1]
+                        operators.removeAt(multiplyIndex)
+                        numbers.removeAt(multiplyIndex)
+                        numbers.removeAt(multiplyIndex)
+                        numbers.add(multiplyIndex, result)
+                    }
+                }
+                result
+            }
+        }.sum()
+        //88534268715686
+        println(result)
     }
 }
